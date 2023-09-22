00527a737ea17956af295c50e275c5c3470c9788
Ivan Baklanov <baklanovivan25@gmail.com>
2023-09-22 20:37:06 +0500

    Added models

--- TestTask/video/models.py
+++ TestTask/video/models.py
@@ -4,3 +4,15 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     name = models.CharField(max_length=50)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=255)
+    video_link = models.URLField()
+    duration_seconds = models.IntegerField()
+    products = models.ManyToManyField(Product)
+
+class LessonView(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    view_time = models.DateTimeField()
+    is_viewed = models.BooleanField(default=False)
